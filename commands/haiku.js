97173e30e3356d0c25ce1fdbed6903ac6ba6e900
Andrew Yurovchak <andy@yurovchak.net>
2024-03-18 09:45:15 -0400

    fix command name

--- commands/haiku.js
+++ commands/haiku.js
@@ -1,28 +1,31 @@
-import { SlashCommandBuilder } from 'discord.js';
-import { completion } from '../utils/openai.js';
-import { logger } from '../utils/logger.js';
+import { SlashCommandBuilder } from "discord.js";
+import { completion } from "../utils/openai.js";
+import { logger } from "../utils/logger.js";
 
-const childLogger = logger.child({ scope: 'haiku.js' });
+const childLogger = logger.child({ scope: "haiku.js" });
 
 export const data = new SlashCommandBuilder()
-	.setName('poem')
-	.setDescription('have the bot write a haiku on the subject of your choice')
-	.addStringOption(option =>
-		option.setName('subject')
-			.setDescription('what do you want the poem to be about?')
+	.setName("haiku")
+	.setDescription("have the bot write a haiku on the subject of your choice")
+	.addStringOption((option) =>
+		option
+			.setName("subject")
+			.setDescription("what do you want the poem to be about?")
 			.setRequired(true)
-			.setMaxLength(100),
+			.setMaxLength(100)
 	);
 
 export async function execute(interaction) {
-	const subject = interaction.options.getString('subject');
+	const subject = interaction.options.getString("subject");
 	await interaction.deferReply();
 	try {
-		const haiku = await completion('write a haiku on the given subject, supply a title surrounded by double asterisks', subject);
+		const haiku = await completion(
+			"write a haiku on the given subject, supply a title surrounded by double asterisks",
+			subject
+		);
 		interaction.editReply(haiku);
-	}
-	catch (error) {
+	} catch (error) {
 		childLogger.error(error);
-		await interaction.editReply('it didnt work sorry \n try again maybe?');
+		await interaction.editReply("it didnt work sorry \n try again maybe?");
 	}
 }
